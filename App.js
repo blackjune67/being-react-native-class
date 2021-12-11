012e07a7d7510cea1144f2fde6cb180b32f56ffa
blackjune67 <buea486@gmail.com>
2021-12-11 16:41:05 +0900

    git remote test

--- App.js
+++ App.js
@@ -1,21 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import React, {Component} from 'react';
+import { StyleSheet, View, Dimensions, Text, ScrollView } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+
+
+
+    render() {
+      return (
+          <View style={styles.container}>
+            <View style={styles.city}>
+              <Text style={styles.cityName}>{city}</Text>
+            </View>
+            <ScrollView
+                pagingEnabled
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={styles.weather}
+            >
+              <View style={styles.day}>
+                <Text style={styles.temp}>27</Text>
+                <Text style={styles.description}>Sunny</Text>
+              </View>
+              <View style={styles.day}>
+                <Text style={styles.temp}>27</Text>
+                <Text style={styles.description}>Sunny</Text>
+              </View>
+              <View style={styles.day}>
+                <Text style={styles.temp}>27</Text>
+                <Text style={styles.description}>Sunny</Text>
+              </View>
+              <View style={styles.day}>
+                <Text style={styles.temp}>27</Text>
+                <Text style={styles.description}>Sunny</Text>
+              </View>
+            </ScrollView>
+          </View>
+      )
+    }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "yellow",
+  },
+  city: {
+    flex: 1.2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 48,
+    fontWeight: "500",
+  },
+  weather: {
+    // backgroundColor: "blue",
+  },
+  day: {
+    width: SCRREN_WIDTH,
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 178,
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 60,
   },
 });
